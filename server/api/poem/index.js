626476cd4da648e618ff9117f84498f5bb4e23e3
emazurek <Edward.R.Mazurek@gmail.com>
2015-07-08 15:33:23 -0400

    Clean up login and API
    Removed login boilerplate and extraneous api routes

--- server/api/poem/index.js
+++ server/api/poem/index.js
@@ -7,10 +7,7 @@ var auth = require('../../auth/auth.service');
 var router = express.Router();
 
 router.get('/', auth.isAuthenticated(), controller.index);
-// router.get('/:userid', controller.show);
 router.post('/', auth.isAuthenticated(), controller.create);
-// router.put('/:id', controller.update);
-// router.patch('/:id', controller.update);
 router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 
 module.exports = router;
